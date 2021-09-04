2440208cbd6c32bd49ea8c4471dca16c4a0ac2dd
Alan Cardiel <acard6@illinois.edu>
2021-09-04 17:10:24 -0500

    code after first test

--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -11,4 +11,11 @@
 #include <sstream>
 
 namespace cs225 {
+    class HSLAPixel{
+        public:
+            double h; // the hue value of the pixel from [0,360]
+            double s; // the saturation of the pixel from [0,1]
+            double l; // the luminance of the pixel from [0,1]
+            double a; // the alpha channel or transparancy of the pixel from [0,1]
+    }
 }
