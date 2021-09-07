a3e5cb0b809a82a5290ecb2279f9fefd6c47eaaf
Alan Cardiel <acard6@illinois.edu>
2021-09-07 17:08:50 -0500

    first bug done

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -29,20 +29,20 @@ PNG* setupOutput(unsigned w, unsigned h) {
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(-1, 0.8, 0.5);
+    HSLAPixel p(250, 0.8, 0.5);
     return &p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
-    original->readFromFile(inputFile);
+    PNG* original = new PNG; //Allocated memory to the pointer
+    std::cout << "Reached line " << __LINE__ << std::endl; //print statement for debugging
+    original-> readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
-
+    std::cout << "Reached line " << __LINE__ << std::endl; //print statement for debugging
     // Create out.png
-    PNG* output;
+    PNG* output = new PNG;
     setupOutput(width, height);
 
     // Load our favorite color to color the outline
@@ -50,16 +50,18 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
+    std::cout << "Reached line " << __LINE__ << std::endl; //print statement for debugging
     for (unsigned y = 1; 0 < y < height; y++) {
         for (unsigned x = 1; 0 < x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
             double diff = std::fabs(curr.h - prev.h);
-
+            std::cout << "y is " << y <<" and x is "<< x <<std::endl;
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel& currOutPixel = (output)->getPixel(x, y);
+            std::cout << "Reached line " << __LINE__ << std::endl; //print statement for debugging
             if (diff > 20) {
                 currOutPixel = *myPixel;
             }
