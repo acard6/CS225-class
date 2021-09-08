00395a6ac164f98b829845735aecfddafa43e038
Alan Cardiel <acard6@illinois.edu>
2021-09-08 00:33:42 -0500

    finished mp

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -25,7 +25,33 @@ void rotate(std::string inputFile, std::string outputFile) {
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
-  // TODO: Part 3
+  cs225::HSLAPixel & origin = png.getPixel(0,0);
+  origin.h = 240;
+  origin.s = 0.7;
+  origin.l = 0.8;
+  for (unsigned x=0; (x<width); x++){
+    for (unsigned y=0; (y<height); y++){
+      cs225::HSLAPixel & curr= png.getPixel(x,y);
+      if ((x % 5) ==0){
+        if ((y%10)==0){
+          curr.h += 60;
+        }
+      }
+      if (curr.h ==360){
+        curr.h = 0;
+      }
+      
+      curr.s += 0.05;
+      if (curr.s ==1){
+        curr.s = 0;
+      }
+      curr.l += 0.05;
+      if (curr.l==1){
+        curr.l =0;
+      
+      } 
+    }
+  }
 
   return png;
 }
